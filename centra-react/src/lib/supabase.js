17ffa0e69f2ff70d4e8827e563f7d2fc34040f26
Krish Parikh <139252908+krish-parikh@users.noreply.github.com>
2025-07-23 19:51:42 +0100

    Update supabase.js

--- centra-react/src/lib/supabase.js
+++ centra-react/src/lib/supabase.js
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://xxpkhvsetnntarhhfzgb.supabase.co";
+const supabaseUrl = "";
 const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh4cGtodnNldG5udGFyaGhmemdiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA2OTk3NDIsImV4cCI6MjA2NjI3NTc0Mn0.BZCnavyl3DkDIr00TEXj_-HjOpAnQVppRvsA5PqrATg";
+  "";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
